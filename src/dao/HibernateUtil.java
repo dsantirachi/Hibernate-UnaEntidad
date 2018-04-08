70e06ee9e4bab57fa6268f7d1cddfe6c6323cda0
dsantirachi <31325393+dsantirachi@users.noreply.github.com>
2018-04-08 17:45:01 -0300

    completando los ejercicios

--- src/dao/HibernateUtil.java
+++ src/dao/HibernateUtil.java
@@ -5,26 +5,27 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 public class HibernateUtil{
-private static SessionFactory sessionFactory ;
-public static SessionFactory getSessionFactory() {
-try {
-if ( sessionFactory == null ) {
-//mediante un objeto Configuration se leerá el fichero de configuración de 
-//hibernate hibernate.cfg.xml  que se encuentra en el directorio raíz de las clases Java.
-Configuration configuration = new Configuration().configure(); 
-//mediante el método configure() se va a leer el fichero de configuración hibernate.cfg.xml.
-ServiceRegistryBuilder registry = new ServiceRegistryBuilder(); 
-registry.applySettings(configuration.getProperties());
-ServiceRegistry serviceRegistry = registry.buildServiceRegistry();
-//Registry contiene la lista de los distintos servicios que usará hibernate
-//nota: esas 3 lineas se pueden escribir como una sola siendo serviceRegistry el resultado final:
-//ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
-}
-} catch (HibernateException he) { System. err .println( "ERROR en la inicialización de la SessionFactory: " + he);
-throw new ExceptionInInitializerError(he);
-}
-return sessionFactory;
-}
+	private static SessionFactory sessionFactory ;
+	public static SessionFactory getSessionFactory() {
+		try {
+			if ( sessionFactory == null ) {
+				//mediante un objeto Configuration se leerá el fichero de configuración de 
+				//hibernate hibernate.cfg.xml  que se encuentra en el directorio raíz de las clases Java.
+				Configuration configuration = new Configuration().configure(); 
+				//mediante el método configure() se va a leer el fichero de configuración hibernate.cfg.xml.
+				ServiceRegistryBuilder registry = new ServiceRegistryBuilder(); 
+				registry.applySettings(configuration.getProperties());
+				ServiceRegistry serviceRegistry = registry.buildServiceRegistry();
+				//Registry contiene la lista de los distintos servicios que usará hibernate
+				//nota: esas 3 lineas se pueden escribir como una sola siendo serviceRegistry el resultado final:
+				//ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+				//la sessionFactory es lo que retorna este metodo y creo q se usa para
+				//hacer transacciones.
+			}
+		} catch (HibernateException he) { System. err .println( "ERROR en la inicialización de la SessionFactory: " + he);
+		throw new ExceptionInInitializerError(he);
+		}
+		return sessionFactory;
+	}
 }
--- src/negocio/ClienteABM.java
+++ src/negocio/ClienteABM.java
@@ -4,34 +4,37 @@ import java.util.List;
 import dao.ClienteDao;
 import datos.Cliente;
 public class ClienteABM {
-ClienteDao dao = new ClienteDao();
-public Cliente traerCliente( long idCliente){
-Cliente c= dao .traerCliente(idCliente);
-// implementar si c es null lanzar Exception
-return c;
-}
-public Cliente traerCliente( int dni){
-Cliente c= dao .traerCliente(dni);
-// implementar si c es null lanzar Exception
-return c;
-}
-public int agregar(String apellido, String nombre, int dni,GregorianCalendar fechaDeNacimiento){
-// consultar si existe un cliente con el mismo dni , si existe arrojar la Excepcion
-Cliente c= new Cliente(apellido, nombre, dni,fechaDeNacimiento);
-return dao .agregar(c);
-}
-public void modificar(Cliente c){
-/* implementar antes de actualizar que no exista un cliente
-con el mismo documento a modificar
-y con el mismo id, lanzar la Exception */
-dao .actualizar(c);
-}
-public void eliminar( long idCliente){ /*en este caso es física en gral. no se se
+	ClienteDao dao = new ClienteDao();
+	public Cliente traerCliente( long idCliente) throws Exception{
+		// implementar si c es null lanzar Exception(HECHO)
+		Cliente c= dao .traerCliente(idCliente);
+		if (c==null) throw new Exception("No se encuentra ningun registro con ese ID.");
+		return c;
+	}
+	public Cliente traerCliente( int dni) throws Exception{
+		// implementar si c es null lanzar Exception(HECHO)
+		Cliente c= dao .traerCliente(dni);
+		if (c==null) throw new Exception("No se encuentra ningun registro con ese DNI.");
+		return c;
+	}
+	public int agregar(String apellido, String nombre, int dni,GregorianCalendar fechaDeNacimiento)throws Exception{
+		// consultar si existe un cliente con el mismo dni , si existe arrojar la Excepcion (HECHO)
+		if(dao.traerCliente(dni)!=null) throw new Exception ("Ya existe un registro con el DNI ingresado.");
+		Cliente c= new Cliente(apellido, nombre, dni,fechaDeNacimiento);
+		return dao .agregar(c);
+	}
+	public void modificar(Cliente c) throws Exception{
+		/* implementar antes de actualizar que no exista un cliente
+		con el mismo documento a modificar y con el mismo id, lanzar la Exception */
+		if(dao.traerCliente(c.getDni())!=null && dao.traerCliente(c.getIdCliente())!=null)
+		dao .actualizar(c);
+	}
+	public void eliminar( long idCliente){ /*en este caso es física en gral. no se se
 aplicaría este caso de uso, si se hiciera habría que validar que el cliente no tenga
 dependencias*/
-Cliente c= dao .traerCliente(idCliente);
-// Implementar que si es null que arroje la excepción la Excepción
-dao .eliminar(c);
-}
-public List<Cliente> traerCliente(){ return dao .traerCliente();}
+		Cliente c= dao .traerCliente(idCliente);
+		// Implementar que si es null que arroje la excepción la Excepción
+		dao .eliminar(c);
+	}
+	public List<Cliente> traerCliente(){ return dao .traerCliente();}
 }
